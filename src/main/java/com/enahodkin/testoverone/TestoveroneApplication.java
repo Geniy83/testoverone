8985a02371b258d683df705c5d64a2bb147659aa
Geniy83 <3477734n@gmail.com>
2022-02-02 09:28:05 +0300

    v1.0

---
+++ src/main/java/com/enahodkin/testoverone/TestoveroneApplication.java
@@ -0,0 +1,13 @@
+package com.enahodkin.testoverone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestoveroneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestoveroneApplication.class, args);
+	}
+
+}
